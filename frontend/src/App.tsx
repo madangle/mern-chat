108bb4c7763902f6d4a881034f7e83eae62886cf
Rishikesh Krishnan <rishikeshnlr@gmail.com>
2025-06-27 21:18:04 +0400

    updated front end page routs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 // import ChatLayout from './components/Layout';
-import HomePage from './Pages/HomePage';
+// import HomePage from './Pages/HomePage';
 import ChatPage from './Pages/ChatPage';
-import Login from './components/authorization/Login';
-import Register from './components/authorization/Register';
+import Register from './Components/authorization/Register.tsx';
+import Login from "./Components/authorization/Login.tsx";
 
 function App() {
   return (
     <>
       <div>
         <Routes>
-          <Route path='/' Component={HomePage}/>
+          <Route path='/' Component={Login}/>
           <Route path='/chat' Component={ChatPage}/>
           <Route path='/login' Component={Login}/>
           <Route path='/sign-up' Component={Register}/>
